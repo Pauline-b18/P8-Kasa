2b1cb19bd2a350d96be4e43b4b5739651b5c49f6
Pauline Borges <pauline.borges18@gmail.com>
2024-04-22 22:08:10 +0200

    component collapse changes

--- src/components/Collapse/Collapse.js
+++ src/components/Collapse/Collapse.js
@@ -1,24 +1,45 @@
-import React, {useState} from "react";
-import arrow_top from '../../assets/arrow_top.png';
+import React, { useState } from "react";
+import PropTypes from 'prop-types';
 import "../../assets/style/components/collapse.scss";
+import arrow_top from '../../assets/arrow_top.png';
+import AboutData from "../../data/AboutData.json";
 
-const Collapse = ({ title, children }) => {
-    const [visible, setVisible] = useState(false);
+const Collapse = ({ title }) => {
+    const [isCollapsed, setIsCollapsed] = useState(true);
 
-    const handleToggle = () => {
-        setVisible((prevVisible) => !prevVisible);
+    const toggleCollapse = () => {
+      setIsCollapsed(!isCollapsed);
     };
 
+    // Recherche l'élément dans AboutData correspondant au titre actuel
+    const currentItem = AboutData.find(item => item.title === title);
+
     return (
-        <div className="CollapseBar">
-            <h3 className="CollapseTitle" onClick={handleToggle} style={{cursor: 'pointer'}}>
-                {title} <img src={arrow_top} alt="Arrow Top" className={`collapse-arrow ${visible ? 'open' : ''}`} />
-            </h3>
-            <div className="collapseContent" style={{display: visible ? 'block' : 'none' }}>
-                {children}
-            </div>
+      <div className='collapse-menu'>
+        <div className='collapse-menu-bar'>
+          <h2 className='collapse-menu-title'>{title}</h2>
+          <img
+            src={arrow_top}
+            alt="Arrow Top"
+            className={`collapse-menu-chevron ${
+              isCollapsed ? '' : 'collapse-menu-chevron-dropdown'
+            }`}
+            onClick={toggleCollapse}
+          />
         </div>
+        <div
+          className={`collapse-menu-content ${
+            isCollapsed ? '' : 'collapse-menu-content-dropdown'
+          }`}
+        >
+          {isCollapsed ? null : currentItem.content}
+        </div>
+      </div>
     );
+};
+
+Collapse.propTypes = {
+    title: PropTypes.string.isRequired,
 };
 
 export default Collapse;
